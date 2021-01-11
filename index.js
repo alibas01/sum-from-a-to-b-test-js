fedbdea5a4db5b93bef52196a51a0d20d9819b81
Ali Bas <alibas01@gmail.com>
2021-01-10 19:35:39 -0500

    function created

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
-}
+  let result = 0;
+  if (toN !== fromN) {
+    result =  toN + sum(fromN, toN - 1);
+    return result;
+  } else {
+    return toN;
+  }
+};
 
 module.exports = sum;
